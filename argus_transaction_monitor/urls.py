7f11ccc6c7904ea96ad3e22e7d2e96eee11321fa
nidhi <modinidhi321@gmail.com>
2026-02-28 17:55:21 +0530

    Update urls in argus_transaction_monitor

--- argus_transaction_monitor/urls.py
+++ argus_transaction_monitor/urls.py
@@ -1,25 +1,7 @@
-"""
-URL configuration for argus_transaction_monitor project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-
-from transactions.views import process_transaction
-
+from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token  # add this
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/transaction/process/', process_transaction),
+    path("", include("transactions.urls")),
+    path("", include("otp_service.urls")),
+    path("api-token-auth/", obtain_auth_token),
 ]
